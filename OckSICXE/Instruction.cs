49b20c3f9b8d944d36e0584e005379cd095684f5
caleb <caleb.ockwig@gmail.com>
2022-10-13 13:02:54 -0500

    Add project files.

---
+++ OckSICXE/Instruction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OckSICXE
+{
+    internal class Instruction
+    {
+        public Instruction()
+        {
+
+        }
+        string Operation { get; set; }
+        string[] Arguments { get; set; }
+        string Comment { get; set; }
+
+
+    }
+}
---
+++ OckSICXE/Opcodes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OckSICXE
+{
+    internal class Opcodes
+    {
+        string Mnemonic { get; set; }
+        string Format { get; set; }
+        byte[] Opcode { get; set; }
+
+        Opcodes(string inputLine)
+        {
+            if(string.IsNullOrEmpty(inputLine))
+            {
+                throw new ArgumentNullException("Input for opcode from opcodes file caused an error. " + inputLine);
+            }
+            inputLine = inputLine.Trim();
+            string[] strings = inputLine.Split(' ');
+            Mnemonic = strings[0];
+            Format = strings[2];
+            char[] ops= strings[1].ToCharArray();
+            Opcode[0] = Byte.Parse("" + ops[0]);
+            Opcode[1] = Byte.Parse("" + ops[1]);
+        }
+    }
+}
